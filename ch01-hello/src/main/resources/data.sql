c5b230d4b32d4c22b9b47cf26b68eb48379f5cf6
maskedmarker <changjianghui0822@gmail.com>
2025-03-23 21:19:02 +0800

    hello中只有最纯粹的最基础的web内容

--- ch01-hello/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into user (name, email) values ('001', '001@qq.com');
-insert into user (name, email) values ('002', '002@qq.com');
-insert into user (name, email) values ('003', '003@qq.com');
-insert into user (name, email) values ('004', '004@qq.com');
-insert into user (name, email) values ('005', '005@qq.com');
