a41756dda2956d9c0d88fae0e92ed974b591577b
maskedmarker <changjianghui0822@gmail.com>
2025-03-29 16:03:46 +0800

    清除proxy-controller

--- ch02-proxy-controller/src/main/java/org/example/learn/spring/boot/web/proxy/autoconfigure/ServiceProxyController.java
+++
@@ -1,7 +0,0 @@
-package org.example.learn.spring.boot.web.proxy.autoconfigure;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public interface ServiceProxyController {
-}
--- ch02-proxy-controller/src/main/java/org/example/learn/spring/boot/web/proxy/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.learn.spring.boot.web.proxy.dao;
-
-import org.example.learn.spring.boot.web.proxy.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
