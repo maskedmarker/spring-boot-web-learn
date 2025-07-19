9d60c482194d31f3704b3e128ebe53ac00fd57a6
maskedmarker <changjianghui0822@gmail.com>
2025-07-19 15:49:16 +0800

    main线程在执行完main方法后,该线程就会结束了.但是web-server的线程还在,所以进程不会自动结束

--- ch01-hello-boot/src/main/java/org/example/learn/spring/boot/web/hello/HelloWebMvcApplication.java
+++ ch01-hello-boot/src/main/java/org/example/learn/spring/boot/web/hello/HelloWebMvcApplication.java
@@ -1,12 +1,17 @@
 package org.example.learn.spring.boot.web.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWebMvcApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloWebMvcApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(HelloWebMvcApplication.class);
+        logger.info("main thread is ending");
     }
 }
